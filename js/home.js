0192c20283128eb4416047edb993ba5ba1359f5a
omar304050 <omaralalfy30@gmail.com>
2024-12-07 20:57:26 +0200

    update one

--- js/home.js
+++ js/home.js
@@ -1,8 +1,12 @@
-let demo = document.querySelector("#demo")
-console.log(demo);
-let users = JSON.parse(localStorage.getItem("list")) || [];
+let demo = document.querySelector("#demo");
+const username = localStorage.getItem("currentUser");
+
+console.log(username);
+
+if (username) {
+    demo.innerHTML = `<h1>Welcome ${username}</h1>`;
+} else {
+    demo.innerHTML = `<h1>Welcome Guest</h1>`;
+}
 
 
-
-
-  demo.innerHTML = `<h1>Welcom</h1>`
--- js/signin.js
+++ js/signin.js
@@ -3,87 +3,57 @@ let passwordSignin = document.querySelector("#passwordSignin");
 let form2 = document.querySelector("#form2");
 let alertValdtion = document.querySelector("#alertValdtion");
 let alertIncorrect = document.querySelector("#alertIncorrect");
-let users = JSON.parse(localStorage.getItem("list")) || [];
 
+let users = JSON.parse(localStorage.getItem("list")) || [];
 console.log(users);
 
-
-
 form2.addEventListener("submit", function (e) {
-    e.preventDefault()
+    e.preventDefault();
     if (passwordValdation() == true && emailValdation() == true) {
+        let findUser = users.find((user) => user.email === emailSignin.value && user.password === passwordSignin.value);
 
-        let findEmail = users.find((user) => user.email === emailSignin.value && user.password === passwordSignin.value)
-        console.log(findEmail);
-
-        if (users.includes(findEmail)) {
-            logIn()
-            alertIncorrect.classList.add("d-none")
+        if (findUser) {
+            logIn(findUser); 
+            alertIncorrect.classList.add("d-none");
         } else {
-            alertIncorrect.classList.remove("d-none")
+            alertIncorrect.classList.remove("d-none");
         }
-
     }
+});
 
-
-
-})
-
-
-function logIn(e) {
-
-    window.location.href = 'home.html'
-
-
+function logIn(user) {
+    localStorage.setItem("currentUser", user.namee);
+    window.location.href = 'home.html';
 }
 
-
-
-
 function emailValdation() {
-    let emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    let emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     if (emailSignin.value == '') {
-        alertValdtion.classList.remove("d-none")
-        alertIncorrect.classList.add("d-none")
+        alertValdtion.classList.remove("d-none");
+        alertIncorrect.classList.add("d-none");
     } else if (emailRegex.test(emailSignin.value) == true) {
-        alertValdtion.classList.add("d-none")
-        alertIncorrect.classList.add("d-none")
-        return true
+        alertValdtion.classList.add("d-none");
+        alertIncorrect.classList.add("d-none");
+        return true;
     } else {
-        alertIncorrect.classList.remove("d-none")
-        alertValdtion.classList.add("d-none")
+        alertIncorrect.classList.remove("d-none");
+        alertValdtion.classList.add("d-none");
     }
-
 }
 
 function passwordValdation() {
-    let passwordRegex = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/
+    let passwordRegex = /^[a-zA-Z0-9!@#$%^&*]{6,16}$/;
 
     if (passwordSignin.value == '') {
-        alertValdtion.classList.remove("d-none")
-        alertIncorrect.classList.add("d-none")
+        alertValdtion.classList.remove("d-none");
+        alertIncorrect.classList.add("d-none");
     } else if (passwordRegex.test(passwordSignin.value) == true) {
-        alertValdtion.classList.add("d-none")
-        alertIncorrect.classList.add("d-none")
-        return true
+        alertValdtion.classList.add("d-none");
+        alertIncorrect.classList.add("d-none");
+        return true;
     } else {
-        alertIncorrect.classList.remove("d-none")
-        alertValdtion.classList.add("d-none")
+        alertIncorrect.classList.remove("d-none");
+        alertValdtion.classList.add("d-none");
     }
-
-
-    // if(passwordRegex.test(passwordSignin.value)==true){
-    //     alertValdtion.classList.add("d-none")
-    //     return true
-    // }else{
-    //     alertValdtion.classList.remove("d-none")
-    // }return false
 }
-
-
-
-
-
-
-
